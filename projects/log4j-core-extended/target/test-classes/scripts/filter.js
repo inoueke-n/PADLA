53e55abcc4eb594d8de7e87b2e5cfa4654a2a5d8
m-tys <m-tys@sel-samba.ics.es.osaka-u.ac.jp>
2019-04-15 13:55:12 +0900

    delete HeijoAgent/bin log4j-core-extended/target and add .gitignore

--- projects/log4j-core-extended/target/test-classes/scripts/filter.js
+++
@@ -1,7 +0,0 @@
-var result = false;
-if (logEvent.getMarker() != null && logEvent.getMarker().isInstanceOf("FLOW")) {
-	result = true;
-} else if (logEvent.getContextData().containsKey("UserId")) {
-	result = true;
-}
-result;
