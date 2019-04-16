d5cd3ba597f300a0ab0844a781698509566063c3
m-tys <m-tys@sel-samba.ics.es.osaka-u.ac.jp>
2019-04-16 17:16:44 +0900

    fix setenv.bat

--- release/apache-tomcat-8.5.34/bin/setenv.bat
+++ release/apache-tomcat-8.5.34/bin/setenv.bat
@@ -4,6 +4,6 @@ rem @SET JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configurationFile=file://%CATALINA_HOME%/
 
 rem To use "Adapter" mode, uncomment the following 2 lines.
 set CATALINA_OPTS=%JAVA_OPTS% -javaagent:"..\HeijoAgent\HeijoAgent.jar=target=jpetstore_plus_tomcat-juli.jar,learningData=..\outputs\vectors.txt,bufferOutput=..\outputs\buffer.txt,buffer=300,interval=5"
-@SET JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configurationFile=file://%CATALINA_HOME%/conf/log4j2_for_AdopterMode.xml
+@SET JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configurationFile=file://%CATALINA_HOME%/conf/log4j2_for_AdapterMode.xml
 
 @echo off
